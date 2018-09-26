002f42f4de8c7c49a561555b9a300c18bac61678
Oliver Paetzel <oliver.paetzel@intranda.com>
2018-09-26 12:56:53 +0200

    finish hotfolder implementation

--- goobi-plugin-administration-hotfolder-tib/src/de/intranda/goobi/plugins/tibhotfolder/QuartzHotfolderJob.java
+++ goobi-plugin-administration-hotfolder-tib/src/de/intranda/goobi/plugins/tibhotfolder/QuartzHotfolderJob.java
@@ -9,14 +9,37 @@ import java.nio.file.Paths;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.nio.file.attribute.FileTime;
 import java.util.Date;
+import java.util.List;
 
 import org.apache.commons.configuration.XMLConfiguration;
+import org.apache.commons.lang.SystemUtils;
+import org.goobi.beans.Process;
+import org.goobi.beans.Step;
+import org.goobi.managedbeans.LoginBean;
+import org.goobi.production.enums.PluginType;
+import org.goobi.production.flow.jobs.HistoryAnalyserJob;
+import org.goobi.production.plugin.PluginLoader;
+import org.goobi.production.plugin.interfaces.IOpacPlugin;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
 import de.sub.goobi.config.ConfigPlugins;
+import de.sub.goobi.helper.BeanHelper;
+import de.sub.goobi.helper.Helper;
+import de.sub.goobi.helper.enums.StepEditType;
+import de.sub.goobi.helper.enums.StepStatus;
+import de.sub.goobi.helper.exceptions.DAOException;
+import de.sub.goobi.helper.exceptions.SwapException;
+import de.sub.goobi.persistence.managers.ProcessManager;
+import de.unigoettingen.sub.search.opac.ConfigOpac;
+import de.unigoettingen.sub.search.opac.ConfigOpacCatalogue;
 import lombok.extern.log4j.Log4j;
+import ugh.dl.Fileformat;
+import ugh.dl.Metadata;
+import ugh.dl.MetadataType;
+import ugh.dl.Prefs;
+import ugh.exceptions.MetadataTypeNotAllowedException;
 
 @Log4j
 public class QuartzHotfolderJob implements Job {
@@ -43,12 +66,47 @@ public class QuartzHotfolderJob implements Job {
                 }
                 try (OutputStream os = Files.newOutputStream(lockFile)) {
                 }
-                //TODO: the dir is done copying. read barcode from it and request catalogue
+                //the dir is done copying. read barcode from it and request catalogue
+                Process p = createNewProcess(dir, config);
+                if (p != null) {
+                    // successfully created process. Copy files to new master-folder
+                    Path masterPath = Paths.get(p.getImagesOrigDirectory(false));
+                    Files.createDirectories(masterPath);
+                    try (DirectoryStream<Path> dirDs = Files.newDirectoryStream(hotFolderPath)) {
+                        for (Path file : dirDs) {
+                            try (OutputStream os = Files.newOutputStream(masterPath.resolve(file.getFileName()))) {
+                                Files.copy(file, os);
+                            }
+                        }
+                    }
+                }
             }
         } catch (IOException e) {
             // TODO Auto-generated catch block
             log.error(e);
+        } catch (InterruptedException | DAOException | SwapException e) {
+            // never happens
         }
+    }
+
+    private Process createNewProcess(Path dir, XMLConfiguration config) {
+        Process template = ProcessManager.getProcessById(config.getInt("templateId"));
+        Prefs prefs = template.getRegelsatz().getPreferences();
+        String barcode = dir.getFileName().toString();
+        ConfigOpacCatalogue coc = ConfigOpac.getInstance().getCatalogueByName("TIB");
+        IOpacPlugin importer = (IOpacPlugin) PluginLoader.getPluginByTitle(PluginType.Opac, "GBV-MARC");
+        Process process = null;
+        try {
+            Fileformat ff = importer.search("12", barcode, coc, prefs);
+            process = cloneTemplate(template);
+            NeuenProzessAnlegen(process, template, ff, prefs);
+
+        } catch (Exception e) {
+            // TODO Write error file to hotfolder error-folder
+            log.error(e);
+            return null;
+        }
+        return process;
     }
 
     private boolean checkIfCopyingDone(Path dir) throws IOException {
@@ -72,6 +130,82 @@ public class QuartzHotfolderJob implements Job {
             }
         }
         return (FIVEMINUTES < smallestDifference) && fileCount > 0;
+    }
+
+    public void NeuenProzessAnlegen(Process process, Process template, Fileformat ff, Prefs prefs) throws Exception {
+
+        for (Step step : process.getSchritteList()) {
+
+            step.setBearbeitungszeitpunkt(process.getErstellungsdatum());
+            step.setEditTypeEnum(StepEditType.AUTOMATIC);
+            LoginBean loginForm = (LoginBean) Helper.getManagedBeanValue("#{LoginForm}");
+            if (loginForm != null) {
+                step.setBearbeitungsbenutzer(loginForm.getMyBenutzer());
+            }
+
+            if (step.getBearbeitungsstatusEnum() == StepStatus.DONE) {
+                step.setBearbeitungsbeginn(process.getErstellungsdatum());
+
+                Date myDate = new Date();
+                step.setBearbeitungszeitpunkt(myDate);
+                step.setBearbeitungsende(myDate);
+            }
+
+        }
+
+        ProcessManager.saveProcess(process);
+
+        /*
+         * -------------------------------- Imagepfad hinzufügen (evtl. vorhandene zunächst löschen) --------------------------------
+         */
+        try {
+            MetadataType mdt = prefs.getMetadataTypeByName("pathimagefiles");
+            List<? extends Metadata> alleImagepfade = ff.getDigitalDocument().getPhysicalDocStruct().getAllMetadataByType(mdt);
+            if (alleImagepfade != null && alleImagepfade.size() > 0) {
+                for (Metadata md : alleImagepfade) {
+                    ff.getDigitalDocument().getPhysicalDocStruct().getAllMetadata().remove(md);
+                }
+            }
+            Metadata newmd = new Metadata(mdt);
+            if (SystemUtils.IS_OS_WINDOWS) {
+                newmd.setValue("file:/" + process.getImagesDirectory() + process.getTitel().trim() + "_tif");
+            } else {
+                newmd.setValue("file://" + process.getImagesDirectory() + process.getTitel().trim() + "_tif");
+            }
+            ff.getDigitalDocument().getPhysicalDocStruct().addMetadata(newmd);
+
+            /* Rdf-File schreiben */
+            process.writeMetadataFile(ff);
+
+        } catch (ugh.exceptions.DocStructHasNoTypeException | MetadataTypeNotAllowedException e) {
+            log.error(e);
+        }
+
+        // Adding process to history
+        HistoryAnalyserJob.updateHistoryForProzess(process);
+
+        ProcessManager.saveProcess(process);
+
+        process.readMetadataFile();
+
+    }
+
+    private Process cloneTemplate(Process template) {
+        Process process = new Process();
+
+        process.setIstTemplate(false);
+        process.setInAuswahllisteAnzeigen(false);
+        process.setProjekt(template.getProjekt());
+        process.setRegelsatz(template.getRegelsatz());
+        process.setDocket(template.getDocket());
+
+        BeanHelper bHelper = new BeanHelper();
+        bHelper.SchritteKopieren(template, process);
+        bHelper.ScanvorlagenKopieren(template, process);
+        bHelper.WerkstueckeKopieren(template, process);
+        bHelper.EigenschaftenKopieren(template, process);
+
+        return process;
     }
 
 }
